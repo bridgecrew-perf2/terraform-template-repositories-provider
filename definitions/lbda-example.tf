c9c262925ea8958a546a9aee2113115a434a171d
Daniel Millan <dani.millan07@gmail.com>
2021-11-09 11:49:46 -0500

    update command to fmt

--- definitions/lbda-example.tf
+++ definitions/lbda-example.tf
@@ -1,8 +1,8 @@
 resource "github_repository" "lbda-example" {
-  name                   = "lbda-example"
-  description            = "Lambda example from Terraform"
-  github_branch_default  = "main"
-  visibility             = "public"
+  name                  = "lbda-example"
+  description           = "Lambda example from Terraform"
+  github_branch_default = "main"
+  visibility            = "public"
 
   template {
     owner      = "danielmillan"
